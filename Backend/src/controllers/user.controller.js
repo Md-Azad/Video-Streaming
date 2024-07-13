61a1f841a3dc72a62a33faf96cabe38ba925017c
Md Azad Hossain <sokalazad@gmail.com>
2024-07-13 02:09:38 +0200

    user input validation and existing user validation checking added

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,11 +1,34 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (req,res) =>{
-    res.status(200).json({
-        message: "ok my coder"
-    })
-})
+const registerUser = asyncHandler(async (req, res) => {
+  // get user from frontend
+  // validation: not empty
+  // check if user exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token fields from response
+  // check for user creation
+  // return response
 
+  // your code here
+  const { username, email, fullname, password } = req.body;
+  console.log("email: ", email);
 
+  if (
+    [username, email, fullname, password].some((field) => field.trim() === "")
+  ) {
+    throw new ApiError(400, "all fields are required");
+  }
 
-export {registerUser};
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(408, "username or email already exists");
+  }
+});
+
+export { registerUser };
